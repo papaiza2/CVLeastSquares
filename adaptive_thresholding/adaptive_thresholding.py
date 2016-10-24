9a84090367361dab1cc0bbbb56f9a507ceda8e5e
papaiza <anton@uniswipe.com>
2016-10-24 16:17:44 -0400

    Changed implementation of adaptive thresholding with average intensity

--- adaptive_thresholding/adaptive_thresholding.py
+++ adaptive_thresholding/adaptive_thresholding.py
@@ -1,18 +1,37 @@
 import sys
 import cv2
+import numpy as np
 
 
 def adaptive_threshold(img, type='adaptive', region=5):
     if type == 'mean':
-        return _mean_thresholding(img, region)
+        return _mean_thresholding(img)
     elif type == 'adaptive':
         return _adaptive_thresholding(img)
     else:
         print "Not a valid thresholding type"
 
-def _mean_thresholding(img, region):
-    return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, region, 3)
 
+def _mean_thresholding(img):
+    sum = 0
+    shape = img.shape
+    for row in range(0, shape[0]):
+        for col in range(0,shape[1]):
+            sum += img[row, col]
+    mean = sum/(shape[0]*shape[1])
+    return threshold(img, mean)
+    # return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, region, 3)
+
+
+def threshold(img, mean):
+    new_img = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+    for row in range(0,img.shape[0]):
+        for col in range(0,img.shape[1]):
+            if img[row, col] > mean:
+                new_img[row, col] = 255
+            else:
+                new_img[row,col] = img[row, col]
+    return img
 
 def _adaptive_thresholding(img):
     pass
@@ -26,7 +45,7 @@ def main():
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        mean_image = adaptive_threshold(gray, type='mean', region=7)
+        mean_image = adaptive_threshold(gray, type='mean')
 
         # Display the resulting frame
         cv2.imshow("Gray", gray)
