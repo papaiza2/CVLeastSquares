0beb055643ec5fd8429c0a3c83da98520efb9ecc
papaiza <anton@uniswipe.com>
2016-11-06 10:32:55 -0500

    Vectorize for loops and do regression testing.

--- adaptive_thresholding/adaptive_thresholding.py
+++ adaptive_thresholding/adaptive_thresholding.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import itertools
 
 
 def adaptive_threshold(img, type='adaptive'):
@@ -41,48 +42,51 @@ def _least_squares_thresholding(img):
     a_32 = a_23 = float((height*(height + 1)/2) * (width*(width + 1)/2))
 
     b_1 = float(cv2.sumElems(img)[0])
-    b_2 = float(height*(height + 1)/2 * b_1)
-    b_3 = float(width*(width + 1)/2 * b_1)
-    b_11 = b_22 = b_33 = 0.0
+    # b_2 = float(height*(height + 1)/2 * b_1)
+    # b_3 = float(width*(width + 1)/2 * b_1)
 
     for j in range(0, width):
         for i in range(0, height):
-            b_11 += float(img[i, j])
-            b_22 += float((i + 1) * img[i, j])
-            b_33 += float((j + 1) * img[i, j])
+            b_2 += float((i + 1) * img[i, j])
+            b_3 += float((j + 1) * img[i, j])
+
 
     matrix = np.array([[a_11, a_12, a_13], [a_21, a_22, a_23], [a_31, a_32, a_33]])
-    answer = np.array([b_11, b_22, b_33])
+    answer = np.array([b_1, b_2, b_3])
 
     unknowns = np.linalg.solve(matrix, answer)
 
     fitted_img = np.zeros((height, width), np.uint8)
     final_img = np.zeros((height, width), np.uint8)
 
-    for j in range(0, width):
-        for i in range(0, height):
-            fitted_img[i, j] = unknowns[0] + unknowns[1]*(i) + unknowns[2]*(j)
+    first = np.dot((unknowns[1] * np.arange(1, height + 1)).reshape(height, 1), np.ones((1, width)))
+    second = np.dot(np.ones((height, 1)), (unknowns[2] * np.arange(1, width+1)).reshape(1, width))
+    fitted_img = abs(unknowns[0] + np.add(first, second))
 
-    for j in range(0, width):
-        for i in range(0, height):
-            if img[i, j] > fitted_img[i, j]:
-                final_img[i, j] = 255
-            else:
-                final_img[i, j] = 0
+    for i, j in itertools.product(range(height), range(width)):
+        # fitted_img[i, j] = unknowns[0] + unknowns[1] * (i + 1) + unknowns[2] * (j + 1)
+        if img[i, j] + 15 > fitted_img[i, j]:
+            final_img[i, j] = 255
+        else:
+            final_img[i, j] = 0
+    # final_img = cv2.absdiff(img, fitted_img.astype(np.uint8))
 
     return fitted_img, final_img
 
 
 def main():
+    # cap = cv2.VideoCapture(0)
+    # while(True):
     img = cv2.imread('../images/shadowed_page.png')
-
+    # ret, frame = cap.read()
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     thresh, new_image = adaptive_threshold(gray, type='adaptive')
 
     # Display the resulting frame
-    cv2.imshow("Gray", gray)
-    cv2.imshow('Thresh', thresh)
+    # cv2.imshow("Gray", gray)
+    # cv2.imshow('Thresh', thresh)
     cv2.imshow('New', new_image)
     cv2.waitKey(0)
 
